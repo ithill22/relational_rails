5ddfe118a0b32b2392bd6dd983ab6512593f8314
Isaac Thill <ithill22@turing.edu>
2023-03-22 12:13:35 -0600

    feat: add routing for team#show and add show action to teams controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
  get "/conferences", to: "conferences#index"
  get "/conferences/:id", to: "conferences#show"
  get "/teams", to: "teams#index"
+ get "/teams/:id", to: "teams#show"
 end
