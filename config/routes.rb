b5215a47503237d447db525cc3be742f37aa2871
Isaac Thill <ithill22@turing.edu>
2023-03-22 12:34:44 -0600

    feat: add routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
  get "/conferences/:id", to: "conferences#show"
  get "/teams", to: "teams#index"
  get "/teams/:id", to: "teams#show"
+ get "/conference/:conference_id/teams", to: "conferences/teams#index"
 end
