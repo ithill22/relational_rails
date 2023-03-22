28539195851f2bb9a9548bc557a7dbe078673463
Isaac Thill <ithill22@turing.edu>
2023-03-22 11:27:29 -0600

    test: add teams and conferences to teams index_spec

--- spec/features/teams/index_spec.rb
+++ spec/features/teams/index_spec.rb
@@ -2,6 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "/teams", type: :feature do
   #user story 3
+  let!(:pac_12) { Conference.create!(name: "Pac-12", 
+                                     region: "Pacific Southwest",
+                                     national_champions: 16,
+                                     power_five: true) }
+  let!(:big_10) { Conference.create!(name: "Big 10",
+                                     region: "Upper Midwest",
+                                     national_champions: 10,
+                                     power_five: true) }
+  let!(:team1) { pac_12.teams.create!(school_name: "University of Colorado",
+                                      mascot: "Buffaloes",
+                                      rank: 25,
+                                      private: false) }
+  let!(:team2) { big_10.teams.create!(school_name: "University of Iowa",
+                                      mascot: "Hawkeyes",
+                                      rank: 13,
+                                      private: false) }                            
   describe "As a visitor, when I visit the /teams page" do
     it "I see the attributes of each team in the system" do
       visit "/teams"
