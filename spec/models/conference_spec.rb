208afbe8d52429c3e186551b2c61dbab0b3f2a8c
Isaac Thill <ithill22@turing.edu>
2023-03-23 11:57:20 -0600

    test: add model test for number of teams

--- spec/models/conference_spec.rb
+++ spec/models/conference_spec.rb
@@ -9,14 +9,28 @@ RSpec.describe Conference, type: :model do
                                      region: "Upper Midwest",
                                      national_champions: 10,
                                      power_five: true) }
+  let!(:team1) { pac_12.teams.create!(school_name: "University of Colorado",
+                                      mascot: "Buffaloes",
+                                      rank: 25,
+                                      private: false) }
+  let!(:team2) { big_10.teams.create!(school_name: "University of Iowa",
+                                      mascot: "Hawkeyes",
+                                      rank: 13,
+                                      private: false) } 
+
   describe "relationships" do
     it { should have_many :teams }
   end
 
   describe 'instance methods' do
-    it '#order_by_created_at' do
+    it '::order_by_created_at' do
 
       expect(Conference.order_by_created_at).to eq(pac_12, big_10)
+    end
+
+    it '#number_of_teams' do
+
+      expect(pac_12.number_of_teams).to eq(1)
     end
   end
 end
