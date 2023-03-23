aa4255f85c702ea03a4a0ec78c9b4b26e08f5f7f
Isaac Thill <ithill22@turing.edu>
2023-03-23 11:22:35 -0600

    test: add test for order_by in conference model spec

--- spec/models/conference_spec.rb
+++ spec/models/conference_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Conference, type: :model do
+  let!(:pac_12) { Conference.create!(name: "Pac-12", 
+                                     region: "Pacific Southwest",
+                                     national_champions: 16,
+                                     power_five: true) }
+  let!(:big_10) { Conference.create!(name: "Big 10",
+                                     region: "Upper Midwest",
+                                     national_champions: 10,
+                                     power_five: true) }
   describe "relationships" do
     it { should have_many :teams }
+  end
+
+  describe 'instance methods' do
+    it '#order_by_created_at' do
+
+      expect(Conference.order_by_created_at).to eq(pac_12, big_10)
+    end
   end
 end
